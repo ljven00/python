649831572fbc76e73ae2b27a83c018eec3427303
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-06-06 18:56:57 -0600

    Complete reverse number challenge

---
+++ log2base2/reverse_num.py
@@ -0,0 +1,14 @@
+"""
+Given an integer,
+return its number backward
+"""
+
+def reverse_num(n: int):
+    """
+    :param n: int to be reversed
+    :return: int the number reversed
+    """
+    temp = reversed(list(str(n)))
+    return int("".join(temp))
+
+print(reverse_num(300))
