5b3e56971fc3d1ac7e9bc1ab86f1f5547127659f
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-06-07 19:27:45 -0600

    Completed sum of array challenge

---
+++ log2base2/sum_arr.py
@@ -0,0 +1,11 @@
+"""
+Given an array of int return
+te sum of the elements
+"""
+
+def sum_arr(arr: list):
+    """
+    :param arr: list of int
+    :return: int sum of the elements
+    """
+    return sum(arr)
