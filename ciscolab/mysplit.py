ef258f2545d151cc2c80c987292b521998d9ced0
ljven00 <jeanvenaldolouissaint@gmail.com>
2023-07-15 08:21:56 -0600

    Complete mysplit function

--- ciscolab/mysplit.py
+++ ciscolab/mysplit.py
@@ -3,28 +3,24 @@ program that behaves like python
 built in split function
 """
 
-def mysplit(s: str, sep = " "):
+def mysplit(string: str, sep = " "):
     """
     splits a given string
     :param s: string to split
     :param sep: separator
     :return: list of separated word
     """
+    word = ""
     string_list = []
-    if sep not in s:
-        string_list.append(s)
-    else:
-        i = 0
-        start = 0
-        max = len(s)
-        while i < max:
-            temp = s.find(sep, i)
-            if temp != -1:
-                sub = s[start:temp]
-                string_list.append(sub)
-                i = temp + 1
-            else:
-                continue
+    for c in string:
+        if c == sep:
+            string_list.append(word)
+            word = ""
+            continue
+        word += c
+    string_list.append(word)
+    # print(string_list)
     return string_list
+
 
 print(mysplit("I love you"))
